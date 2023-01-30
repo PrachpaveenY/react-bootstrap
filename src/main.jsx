2fc4a70c1b1f4b012de43261c13af1eca82d7b14
Prachpaveen Y <prachpaveen888@gmail.com>
2023-01-30 16:18:29 +0700

    update App 30/01/2566

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import NavBar from './Components/NavBar'
+import Homepage from './Page/Homepage'
+import ProductList from './Page/ProductList'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <BrowserRouter>
+        <div>
+          <NavBar/>
+          <Route path="/" exact={true} Component={Homepage}/>
+          <Route path="/productlist" Component={ProductList}/>
+        </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
